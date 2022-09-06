794e047e76ad1aace000a4e4002cc88b32350bbc
hiroboo7531 <hiroyosi14e2132@gmail.com>
2022-09-06 15:09:42 +0900

    商品詳細編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
+
+  before_action :correct_signin_user, only: [:edit , :update]
+  # 出品者以外のログイン者がURLをいじって商品編集ページに飛べないようにするよ
+  # , :destroy, :update]
+
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -24,10 +29,35 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+      
+    else
+      render :edit
+    end
+  end
+
+
+ 
+
 
   private
 
   def item_params
     params.require(:item).permit(:item_name,:explanation,:category_id,:condition_id,:send_fee_id,:prefecture_id,:delivery_date_id,:price, :image).merge(user_id: current_user.id)
+  end
+
+  def correct_signin_user
+    if user_signed_in? 
+    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user == @item.user
+    else
+    redirect_to new_user_session_path
+    end
   end
 end
